f462e4ad7e1715b6db49e6159d4fe996a25a556f
Prashanth Konduru <konduruprashanth0@gmail.com>
2026-08-04 16:08:26 +0530

    Completed Python List Task

---
+++ README.md
@@ -0,0 +1,12 @@
+# Python List Task 
+
+# Objective
+Create a Python program to perform basic list operations.
+# Description 
+This program:
+- Creates a list of numbers 
+- Calculates the sum of the numbers 
+- Calculates the average 
+- Finds the maximum value 
+- Finds the minimum value 
+- Displays all the results using Python-built in functions  
---
+++ task.py
@@ -0,0 +1,22 @@
+# List of numbers 
+numbers = [10, 20, 30 , 40, 50]
+# Calculate sum
+total = sum(numbers)
+# calculate average 
+average = total/len(numbers)
+# Find maximum value
+maximum = max(numbers)
+# Find minimum value
+minimum = min(numbers)
+# Display results 
+print("List of numbers:",numbers)
+print("Sum:", total)
+print("Average:", average)
+print("Maximum:", maximum)
+print("Minimum:", minimum)
+
+
+
+
+
+
